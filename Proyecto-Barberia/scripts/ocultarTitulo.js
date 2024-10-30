4ecebe5e0be2c25823a55950e361adcb0b1d2b62
ivanuc21 <ivanuc172021@gmail.com>
2024-10-30 15:42:54 -0300

    Delete Proyecto-Barberia directory

--- Proyecto-Barberia/scripts/ocultarTitulo.js
+++
@@ -1,8 +0,0 @@
-let titulo = document.getElementById('titulo');
-if (titulo.style.display === '') {
-    titulo.style.display = 'block'; 
-}
-
-let check = document.getElementById('check').addEventListener('click', function() {
-    titulo.style.display = (titulo.style.display === 'block') ? 'none' : 'block';
-});
